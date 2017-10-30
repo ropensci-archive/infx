33b82d5b54478db642bad8c5df71c8a17118ad09
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-30 12:47:25 +0100

    add plate fetcher

--- R/openbis.R
+++ R/openbis.R
@@ -6,19 +6,19 @@
 #' openBis instance.
 #' 
 #' @param username,password OpneBis login credentials
-#' @param data_type Sepcify the type of dataset to be downloaded
+#' @param data_type Specify the type of dataset to be downloaded
 #' @param data_id If applicable, for example for a single aggregate file,
 #' directly select the single dataset to download (optional)
 #' @param plate_regex If datasets corresponding to one or several plates are
 #' targeted, they can be selected via a regular expression (optional)
-#' @param file_regex A regular expression appled to filenames can be used to
+#' @param file_regex A regular expression applied to filenames can be used to
 #' filter the selection (optional)
 #' @param out_dir Directory used to save the downloaded data to (default: the
 #' current working dir)
 #' @param result_class A string, indicating the result class to restrict to.
 #' NULL to skip.
 #' @param newest Logical, indicating whether to only return the newest
-#' version af a matched dataset per plate (default is TRUE).
+#' version as a matched dataset per plate (default is TRUE).
 #' @param verbosity The verbosity level of BeeDataSetDownloader (an integer in
 #' [0-25]) 
 #' 
@@ -56,9 +56,12 @@ fetch_openbis <- function(username = "rdgr2014",
       "--type", paste0("'", data_type, "'"),
     if (!is.null(result_class))
       c("--result-class", paste0("'", result_class, "'")),
-    if (!is.null(data_id)) c("--datasetid", paste0("'", data_id, "'")),
-    if (!is.null(plate_regex)) c("--plateid", paste0("'", plate_regex, "'")),
-    if (newest) "--newest",
+    if (!is.null(data_id))
+      c("--datasetid", paste0("'", data_id, "'")),
+    if (!is.null(plate_regex))
+      c("--plateid", paste0("'", plate_regex, "'")),
+    if (newest)
+      "--newest",
     "--files", paste0("'", file_regex, "'"),
     "--outputdir", paste0("'", out_dir, "'"))
 
@@ -71,4 +74,80 @@ fetch_openbis <- function(username = "rdgr2014",
     stop(paste(ret, collapse = "\n"))
 
   ret
+}
+
+#' @title Fetch single cell data
+#'
+#' @description Using [fetch_openbis()], this function downloads single cell
+#' datasets corresponding to a plate barcode and filtered by an optional file
+#' name regular expression. The downloaded data is either saved to a user
+#' specified directory or to a temporary location which can be auto removed.
+#' 
+#' @param plate_name The plate barcode of interest.
+#' @param auto_rm A logical switch; if TRUE, all downloaded files will be
+#' removed upon garbage collection of the returned character vector.
+#' @param ... Arguments passed to [fetch_openbis()]
+#' 
+#' @return A vector of downloaded filenames.
+#' 
+#' @export
+#' 
+fetch_plate <- function(plate_name,
+                        auto_rm = TRUE,
+                        ...) {
+
+  dots <- list(...)
+
+  stopifnot(is.null(dots$plate_regex))
+  dots$plate_regex <- paste0("^/.*/.*/.*/", plate_name, "$")
+
+  if (!is.null(dots$data_type))
+    warning("ignoring the argument \"data_type\".")
+  dots$data_type <- "HCS_ANALYSIS_CELL_FEATURES_CC_MAT"
+
+  if (is.null(dots$file_regex))
+    dots$file_regex <- ".*\\.mat$"
+  if (is.null(dots$out_dir))
+    dots$out_dir <- tempfile()
+
+  if (dir.exists(dots$out_dir)) {
+    files <- list.files(dots$out_dir, pattern = dots$file_regex,
+                        recursive = TRUE)
+    if (length(files) > 1) {
+      message("found ", length(files), " files under\n  ",
+              normalizePath(dots$out_dir), "\n  ",
+              "please remove/change cacheDir if this is not what is desired.")
+      return(files)
+    } else {
+      stop("could not find any files under\n  ", normalizePath(dots$out_dir),
+           "\n  please remove/change cacheDir.")
+    }
+  }
+
+  dir.create(dots$out_dir)
+
+  out <- do.call(fetch_openbis, dots)
+
+  stopifnot(out[4] == "Login successful")
+  stopifnot(out[length(out)] == paste0("Module BeeDataSetDownloader ",
+                                       "finished successfully"))
+  files <- out[grepl("^The download '.+' does not exist, will download.$",
+                     out)]
+  stopifnot(length(files) >= 1)
+  files <- gsub("The download '", "",
+                gsub("' does not exist, will download.$", "", files))
+
+  stopifnot(all(sapply(files, file.exists)))
+
+  if (auto_rm) {
+    attr(files, "finaliser") <- (function(dir) {
+      reg.finalizer(environment(), function(...) {
+        message("Auto-removing downloaded files from\n", dir)
+        unlink(dir, recursive = TRUE)
+      }, onexit = TRUE)
+      environment()
+    })(dots$out_dir)
+  }
+
+  files
 }
--- tests/testthat/test-openbis.R
+++ tests/testthat/test-openbis.R
@@ -1,15 +1,42 @@
 context("openbis downloader")
 
-test_that("openbis system call", {
+test_that("openbis system call works", {
   expect_error(fetch_obis())
   dir <- tempfile()
   expect_true(dir.create(dir))
   tmp <- fetch_openbis(data_id = "20150421161324410-3129549", out_dir = dir)
-  expect_true(any(grepl("^Successfully downloaded 2 files", tmp)))
+  expect_match(tmp, "^Successfully downloaded 2 files", all = FALSE)
   tmp <- fetch_openbis(plate_regex = "^/.*/BB02-2E$",
                        data_type = "HCS_ANALYSIS_CELL_FEATURES_CC_MAT",
                        file_regex = ".*Children_[A-z]+_Count.mat$",
                        out_dir = dir)
-  expect_true(any(grepl("does not exist, will download.$", tmp)))
+  expect_match(tmp, "does not exist, will download.$", all = FALSE)
   unlink(dir, recursive = TRUE)
+})
+
+test_that("plates can be fetched", {
+  dir <- tempfile()
+  expect_error(fetch_plate("BB02-2E", plate_regex = "foo", out_dir = dir))
+  expect_warning(fetch_plate("BB02-2E", data_type = "foo",
+                 file_regex = ".*Children_[A-z]+_Count.mat$", out_dir = dir))
+  unlink(dir, recursive = TRUE)
+  expect_error(fetch_plate("BB02-2E", file_regex = "foo", out_dir = dir))
+  unlink(dir, recursive = TRUE)
+  expect_true(dir.create(dir))
+  expect_error(fetch_plate("BB02-2E",
+                           file_regex = ".*Children_[A-z]+_Count.mat$",
+                           out_dir = dir))
+  unlink(dir, recursive = TRUE)
+  dat <- fetch_plate("BB02-2E",
+                     file_regex = ".*Children_[A-z]+_Count.mat$",
+                     out_dir = dir)
+  expect_length(dat, 4L)
+  expect_match(dat, ".*Children_[A-z]+_Count.mat$", all = TRUE)
+  expect_match(list.files(dir, recursive = TRUE),
+               ".*Children_[A-z]+_Count.mat$", all = TRUE)
+  expect_message(fetch_plate("BB02-2E", out_dir = dir),
+                 "^found 4 files under")
+  rm(dat)
+  gc()
+  expect_false(dir.exists(dir))
 })
