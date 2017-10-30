35e204ab43868c172e2b69b943061c3918809504
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-30 10:24:44 +0100

    add travis

--- tests/testthat/test-openbis.R
+++ tests/testthat/test-openbis.R
@@ -6,9 +6,10 @@ test_that("openbis system call", {
   expect_true(dir.create(dir))
   tmp <- fetch_openbis(data_id = "20150421161324410-3129549", out_dir = dir)
   expect_true(any(grepl("^Successfully downloaded 2 files", tmp)))
-  tmp <- fetch_openbis(plate_id = "^/.*/BB02-2E$",
+  tmp <- fetch_openbis(plate_regex = "^/.*/BB02-2E$",
                        data_type = "HCS_ANALYSIS_CELL_FEATURES_CC_MAT",
                        file_regex = ".*Children_[A-z]+_Count.mat$",
                        out_dir = dir)
   expect_true(any(grepl("does not exist, will download.$", tmp)))
+  unlink(dir, recursive = TRUE)
 })
