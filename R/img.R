c5b69e99950f9dfeab61a16e30dbcf73fbedd827
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-11-17 19:10:34 +0100

    add raw/segmentation img datasets

--- R/img.R
+++ R/img.R
@@ -9,13 +9,23 @@
 #' @inheritParams logout_openbis
 #' @param plate_id Either a character vector holding plate barcodes or a list
 #' of/single PlateIdentifier object(s).
+#' @param type A switch to choose from raw or segmentation image datasets.
+#' @param most_recent Logical switch, if TRUE only the most recent dataset is
+#' returned.
 #' 
 #' @return A list/single object of type ImageDatasetReference.
 #' 
 #' @export
 #' 
 list_img_datasets <- function(token,
-                              plate_id) {
+                              plate_id,
+                              type = c("raw", "segmentation"),
+                              most_recent = TRUE) {
+
+  type <- match.arg(type)
+  type <- switch(type,
+                 raw = "listRawImageDatasets",
+                 segmentation = "listSegmentationImageDatasets")
 
   if (all(sapply(plate_id, is.character)))
     plate_id <- lapply(plate_id, create_plate_id, token = token)
@@ -25,6 +35,10 @@ list_img_datasets <- function(token,
   assert_that(is.list(plate_id),
               all(sapply(plate_id, has_json_class, "PlateIdentifier")))
 
-  query_openbis("listImageDatasets", list(token, plate_id),
-                "IScreeningApiServer")
+  res <- query_openbis(type, list(token, plate_id), "IScreeningApiServer")
+
+  if (most_recent)
+    res[[which.max(sapply(res, `[[`, "registrationDate"))]]
+  else
+    res
 }
--- tests/testthat/test-img.R
+++ tests/testthat/test-img.R
@@ -2,6 +2,14 @@ context("image tools")
 
 test_that("image datasets can be listed", {
   expect_is(img_ds <- list_img_datasets(tok, "KB03-2K"), "list")
-  expect_true(all(sapply(img_ds, has_json_class, "ImageDatasetReference")))
+  expect_true(has_json_class(img_ds, "ImageDatasetReference"))
   expect_gte(length(img_ds), 1L)
+  expect_is(segm_ds <- list_img_datasets(tok, "KB03-2K", "segmentation"),
+            "list")
+  expect_true(has_json_class(segm_ds, "ImageDatasetReference"))
+  expect_gte(length(segm_ds), 1L)
+  expect_is(segm_ds <- list_img_datasets(tok, "KB03-2K", "segmentation",
+                                         FALSE), "list")
+  expect_true(all(sapply(segm_ds, has_json_class, "ImageDatasetReference")))
+  expect_gte(length(segm_ds), 1L)
 })
