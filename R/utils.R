65df1a7d36fd0115eb11ba99c1067e1fb80e9ff3
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-05 15:34:11 +0100

    update request docs

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,62 @@
 
-#' @title Helper querying openBis API
+#' Make a JSON-RPC request
 #'
-#' @description Issues a POST request to the JSON-RPC based openBis API v1,
-#' using [make_request]. Documentation is available at
-#' \code{https://wiki-bsse.ethz.ch/display/openBISDoc/openBIS+JSON+API}.
+#' Issues a POST request to a JSON-RPC server. All `@type` fields are
+#' converted to/from `json_class` attributes, using [as_json_list()] and
+#' [as_json_class()]. The helper function `query_openbis()` wraps
+#' `make_request()` and constructs the url the request is sent to.
 #' 
-#' @inheritParams make_request
-#' @param api The api section the used method is part of. This is used to
-#' construct the url the request is sent to.
-#' @param host The base url, the request is sent to.
+#' @param url Destination url, the request is sent to.
+#' @param api, host Strings used to construct the destination url.
+#' @param method The API method name.
+#' @param params A list structure holding the arguments which, converted to
+#' JSON, will be used to call the supplied method. The `@type` entries will be
+#' generated from `json_class` attributes.
+#' @param version JSON-RPC protocol version to be used (defaults to `"2.0"`.
+#' @param id An identifier for the JSON-RPC request (defaults to `"1"`). Can be
+#' ignored, as only single JSON-RPC requests are issued per HTTP request.
 #' 
-#' @return A list/data.frame holding the response from openBis.
+#' @return A (nested) list holding the response from the JSON-RPC server
+#' (`@type` entries are converted to `json_class` attributes).
+#' 
+#' @rdname request
+#' 
+#' @examples
+#' \donttest{
+#'   tok <- login_openbis("rdgr2014", "IXPubReview")
+#'   projects <- query_openbis("listProjects", tok)
+#'   print(projects[[1]])
+#' }
+#' 
+make_request <- function(url,
+                         method,
+                         params,
+                         version = "2.0",
+                         id = "1") {
+
+  if (!is.list(params)) params <- list(params)
+
+  req <- list(id = id,
+              jsonrpc = version,
+              method = method,
+              params = as_json_list(params))
+
+  res <- httr::POST(url, body = req, encode = "json")
+
+  assert_that(res$status_code == 200)
+
+  res$content <- jsonlite::fromJSON(rawToChar(res$content),
+                                    simplifyVector = FALSE)
+
+  if (!is.null(res$content$error))
+    stop("Error:\n", paste(names(res$content$error), res$content$error,
+                           sep = ": ", collapse = "\n"))
+
+  as_json_class(res$content$result)
+}
+
+#' @rdname request
+#' @export
 #' 
 query_openbis <- function(method,
                           params,
@@ -42,45 +88,4 @@ query_openbis <- function(method,
                   "rmi-datastore-server-screening-api-v1.json")
 
   make_request(paste(host, url, sep = "/"), method, params)
-}
-
-#' @title Make a JSON-RPC request
-#'
-#' @description Issues a POST request to a JSON-RPC server. All "@type" fields
-#' are converted to/from "json_class" attributes.
-#' 
-#' @param url Url, the request is sent to.
-#' @param method The method name
-#' @param params A list structure holding the arguments which, converted to
-#' JSON, will be used to call the supplied method. The "@type" entries will be
-#' generated from "json_class" attributes.
-#' @param version JSON-RPC protocol version to be used.
-#' @param id Id of the JSON-RPC request.
-#' 
-#' @return A (nested) list holding the response from the JSON-RPC server
-#' ("@type" entries are converted to "json_class" attributes).
-#' 
-make_request <- function(url,
-                         method,
-                         params,
-                         version = "2.0",
-                         id = "1") {
-
-  req <- list(id = id,
-              jsonrpc = version,
-              method = method,
-              params = as_json_list(params))
-
-  res <- httr::POST(url, body = req, encode = "json")
-
-  assert_that(res$status_code == 200)
-
-  res$content <- jsonlite::fromJSON(rawToChar(res$content),
-                                    simplifyVector = FALSE)
-
-  if (!is.null(res$content$error))
-    stop("Error:\n", paste(names(res$content$error), res$content$error,
-                           sep = ": ", collapse = "\n"))
-
-  as_json_class(res$content$result)
 }
