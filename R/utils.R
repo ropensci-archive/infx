28c6c7fe58f7f33c56fe424ee6295edfebe35f4f
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-07 19:07:27 +0100

    fix `error_default()` docs

--- R/utils.R
+++ R/utils.R
@@ -2,13 +2,14 @@
 #' Helper for S3 default error
 #'
 #' Whenever the default function of an S3 generic is a `not implemented` error,
-#' tis function can be used.
+#' this function can be used. It is a wrapper around [base::stop()], with a
+#' custom error message.
 #' 
 #' @param x The object on which the dispatch is done.
 #' 
 #' @keywords internal
 #' 
-error_default <- function(x, ...) {
+error_default <- function(x) {
   stop("cannot handle objects of type c(",
        paste0("\"", class(x), "\"", collapse = ", "), ").")
 }
