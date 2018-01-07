4f722d781ab31fba1e079e13a8108eb6d9cff553
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-07 16:28:37 +0100

    fix cran warning

--- R/dataset.R
+++ R/dataset.R
@@ -1,4 +1,12 @@
 
+#' List dataset types
+#'
+#' Given a login token, all available dataset types are listed.
+#' 
+#' @inheritParams logout_openbis
+#' 
+#' @return A a list of DataSetType objects.
+#' 
 #' @export
 #' 
 list_dataset_types <- function(token)
