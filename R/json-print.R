36d226fc1872b8b65d0eddccdaa578bbad247d77
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-15 15:02:01 +0100

    move json printing to separate file

---
+++ R/json-print.R
@@ -0,0 +1,228 @@
+
+#' Print JSON class objects
+#'
+#' Inspired by the ast printing function of Hadley's `lobstr` package and
+#' borrowing code from [there](https://git.io/vFMA5), this enables the
+#' recursive printing of `json_class` and `json_vec` objects. Printing style
+#' can either be with fancy (colors, UTF box characters. etc.) or simple and
+#' several options are available for setting the max printing with/length, as
+#' well as a max recursion depth for nested `json_class` objects.
+#' 
+#' @param x Object to print.
+#' @param depth The maximum recursion depth for printing.
+#' @param width Number of columns to maximally print.
+#' @param length Number of lines to maximally print.
+#' @param fancy Logical switch to enable font styles, colors and UTF box
+#' characters for printing.
+#' @param ... Generic compatibility.
+#' 
+#' @rdname json_print
+#'  
+#' @examples
+#' tmp <- json_class(a = json_class(b = "c", class = "foo"),
+#'                   d = json_class(e = "f", class = "bar"),
+#'                   class = "foobar")
+#' tmp
+#' print(tmp, depth = 2L)
+#' print(tmp, depth = 2L, length = 4L)
+#' 
+#' @export
+#' 
+print.json_class <- function(x,
+                             depth = 1L,
+                             width = getOption("width"),
+                             length = 100L,
+                             fancy = TRUE,
+                             ...) {
+
+  if (!check_json_class(x))
+    warning("printing a json_class object that is not properly formed.")
+
+  out <- print_json_class(x, cur_depth = 0L, max_depth = depth,
+                          layout = style(fancy))
+
+  too_wide <- crayon::col_nchar(out) > width
+  out[too_wide] <- paste0(crayon::col_substr(out[too_wide], 1L,
+                                             width - 4L), "...")
+
+  if (length(out) > length) {
+    out[length] <- "..."
+    out <- out[seq_len(length)]
+  }
+
+  cat(paste(out, "\n", collapse = ""), sep = "")
+  invisible(x)
+}
+
+#' @rdname json_print
+#' @export
+#' 
+print.json_vec <- function(x,
+                           depth = 1L,
+                           width = getOption("width"),
+                           length = 100L,
+                           fancy = TRUE,
+                           ...) {
+
+  form <- style(fancy)
+
+  out <- lapply(x, print_json_class, cur_depth = 0L, max_depth = depth,
+                layout = form)
+
+  if (length(out) == 1L) {
+    out <- c(paste0(form$h, form$h, out[[1]][1]),
+             paste(" ", out[[1]][-1]))
+  } else {
+    out <- c(paste0(form$t, form$h, out[[1]][1]),
+             paste(form$v, out[[1]][-1]),
+             unlist(lapply(out[-c(1, length(out))], function(y) {
+               c(paste0(form$j, form$h, y[1]), paste(form$v, y[-1]))
+             })),
+             paste0(form$l, form$h, out[[length(out)]][1][1]),
+             paste(" ", out[[length(out)]][-1]))
+  }
+
+  too_wide <- crayon::col_nchar(out) > width
+  out[too_wide] <- paste0(crayon::col_substr(out[too_wide], 1L,
+                                             width - 3L), "...")
+
+  if (length(out) > length) {
+    out[length] <- "..."
+    out <- out[seq_len(length)]
+  }
+
+  cat(paste(out, "\n", collapse = ""), sep = "")
+  invisible(x)
+}
+
+# Helper functions for printing JSON objects
+#
+# @param x The JSON object to print.
+# @param unnamed_parent Whether the parent node is named or not (in some
+# cases, a different box character has to be used if this is true).
+# @param cur_depth The current recursion depth.
+# @param max_depth The maximum recursion depth.
+# @param layout Characters for printing the tree structure and styles to be
+# applied to the different entities.
+# @param fancy Logical switch to enable font styles, colors and UTF box
+# characters.
+# 
+print_json_class <- function(x,
+                             unnamed_parent = FALSE,
+                             cur_depth,
+                             max_depth,
+                             layout = style()) {
+
+  indent <- function(x, first, rest) {
+    if (length(x) == 1)
+      paste0(first, x)
+    else
+      c(paste0(first, x[[1]]), paste0(rest, x[-1L]))
+  }
+
+  if (!is.list(x)) {
+
+    if (length(x) > 1L)
+      layout$val(paste0("(", paste(x, collapse = ", "), ")"))
+    else
+      layout$val(paste(x))
+
+  } else {
+
+    if (is.null(names(x)))
+      nme <- rep("", length(x))
+    else {
+      nme <- paste(names(x), "= ")
+      nme[names(x) == ""] <- ""
+    }
+
+    if (!is_json_class(x) && !any(sapply(x, is.list))) {
+
+      layout$val(paste0("[", paste0(nme, x, collapse = ", "), "]"))
+
+    } else {
+
+      cur_depth <- cur_depth + 1
+
+      if (cur_depth <= max_depth) {
+
+        if (any(sapply(x, is.null) | sapply(x, length) == 0L))
+          x[sapply(x, is.null) | sapply(x, length) == 0L] <- ""
+
+        rest <- Map(indent,
+                    mapply(print_json_class, x, nme == "",
+                           MoreArgs = list(cur_depth = cur_depth,
+                                           max_depth = max_depth,
+                                           layout = layout), SIMPLIFY = FALSE),
+                    layout$key(nme),
+                    strrep(" ", nchar(nme)))
+      } else
+        rest <- "..."
+
+      if (is_json_class(x)) {
+        c(indent(layout$obj(class(x)[1]), paste0(layout$n, layout$h),
+                            paste0(layout$v,  " ")),
+          unlist(lapply(rest[-length(rest)], indent,
+                        paste0(layout$j, layout$h), paste0(layout$v,  " "))),
+          indent(rest[[length(rest)]], paste0(layout$l, layout$h), "  ")
+        )
+      } else if (cur_depth <= max_depth) {
+        c(indent(rest[[1]], paste0(if (unnamed_parent) layout$c else layout$t,
+                                   layout$h), paste0(layout$v,  " ")),
+          unlist(lapply(rest[-c(1, length(rest))], indent,
+                        paste0(layout$j, layout$h), paste0(layout$v,  " "))),
+          indent(rest[[length(rest)]], paste0(layout$l, layout$h), "  ")
+        )
+      } else {
+        "..."
+      }
+    }
+  }
+}
+
+style <- function(fancy = TRUE) {
+
+  if (fancy && l10n_info()$`UTF-8`) {
+
+    box <- list(h = "\u2500", # ─ horizontal
+                t = "\u250C", # ┌ top
+                v = "\u2502", # │ vertical
+                l = "\u2514", # └ leaf
+                c = "\u252C", # ┬ junction
+                j = "\u251C", # ├ junction
+                n = "\u2588") # █ node
+
+  } else {
+
+    box <- list(h = "-",
+                t = "//",
+                v = "|",
+                l = "\\",
+                c = "+",
+                j = "+",
+                n = "X")
+  }
+
+  if (fancy && crayon::has_color()) {
+
+    # nocov start
+    obj_col <- crayon::magenta
+    key_col <- crayon::silver
+    val_col <- crayon::yellow
+
+    box$n <- obj_col(box$n)
+
+    c(box,
+      obj = crayon::combine_styles(crayon::underline, obj_col),
+      key = crayon::combine_styles(crayon::italic, key_col),
+      val = val_col)
+    # nocov end
+
+  } else {
+
+    c(box,
+      obj = identity,
+      key = identity,
+      val = identity)
+  }
+}
--- R/json-vec.R
+++ R/json-vec.R
@@ -18,11 +18,6 @@
 #'
 #' @param ... Individual `json_class` objects, or generic compatibility
 #' @param x A single/list of `json_class` object(s), or other object to coerce
-#' @param depth The maximum recursion depth for printing.
-#' @param width Number of columns to maximally print.
-#' @param length Number of lines to maximally print.
-#' @param fancy Logical switch to enable font styles, colors and UTF box
-#' characters for printing.
 #' 
 #' @examples
 #' a <- structure(list("a"), class = c("foo", "json_class"))
@@ -199,44 +194,3 @@ is_json_vec <- function(x) {
 #' @export
 #' 
 is.json_vec <- is_json_vec
-
-#' @rdname json_vec
-#' @export
-#' 
-print.json_vec <- function(x,
-                           depth = 1L,
-                           width = getOption("width"),
-                           length = 100L,
-                           fancy = TRUE,
-                           ...) {
-
-  form <- style(fancy)
-
-  out <- lapply(x, print_json_class, cur_depth = 0L, max_depth = depth,
-                layout = form)
-
-  if (length(out) == 1L) {
-    out <- c(paste0(form$h, form$h, out[[1]][1]),
-             paste(" ", out[[1]][-1]))
-  } else {
-    out <- c(paste0(form$t, form$h, out[[1]][1]),
-             paste(form$v, out[[1]][-1]),
-             unlist(lapply(out[-c(1, length(out))], function(y) {
-               c(paste0(form$j, form$h, y[1]), paste(form$v, y[-1]))
-             })),
-             paste0(form$l, form$h, out[[length(out)]][1][1]),
-             paste(" ", out[[length(out)]][-1]))
-  }
-
-  too_wide <- crayon::col_nchar(out) > width
-  out[too_wide] <- paste0(crayon::col_substr(out[too_wide], 1L,
-                                             width - 3L), "...")
-
-  if (length(out) > length) {
-    out[length] <- "..."
-    out <- out[seq_len(length)]
-  }
-
-  cat(paste(out, "\n", collapse = ""), sep = "")
-  invisible(x)
-}
