499e0eaa2fdce4025294dfa80479cc289d5d1034
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-16 19:02:53 +0100

    add ds/sample type tests

--- tests/testthat/test-dataset.R
+++ tests/testthat/test-dataset.R
@@ -5,4 +5,6 @@ test_that("dataset types can be listed", {
   expect_is(ds_types, "DataSetType")
   expect_is(ds_types, "json_vec")
   expect_identical(get_common_subclass(ds_types), "DataSetType")
+  expect_true(all(sapply(ds_types, has_json_subclass, "DataSetType")))
+  expect_gte(length(ds_types), 1L)
 })
--- tests/testthat/test-sample.R
+++ tests/testthat/test-sample.R
@@ -5,4 +5,6 @@ test_that("sample types can be listed", {
   expect_is(samp_types, "SampleType")
   expect_is(samp_types, "json_vec")
   expect_identical(get_common_subclass(samp_types), "SampleType")
+  expect_true(all(sapply(samp_types, has_json_subclass, "SampleType")))
+  expect_gte(length(samp_types), 1L)
 })
