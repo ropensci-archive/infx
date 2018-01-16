c2ee4a94fb21b8205b86d67dcd3a6f8041fcd582
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-16 11:30:45 +0100

    do not export as_list; test for pkgdown

--- R/infx.R
+++ R/infx.R
@@ -2,16 +2,4 @@
 #' @import assertthat
 NULL
 
-#' Alias for `as.list()`
-#'
-#' For consistency with other functions provided by this package, where for
-#' example both `is_json_vec()` and `is.json_vec()` are available, `as_list()`
-#' is provided as an alias for `as.list()`. Consequently, functions such as
-#' `as.list.json_vec()` are also available as `as_list.json_vec()`.
-#' 
-#' @param x Object to coerce to list.
-#' @param ... Generic compatibility.
-#' 
-#' @export
-#' 
 as_list <- as.list
