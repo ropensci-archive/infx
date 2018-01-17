8f71c7ff3fc02ac1989db046306c3e2663fbadbf
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-17 12:47:29 +0100

    add exp id lister

--- R/experiment.R
+++ R/experiment.R
@@ -11,3 +11,16 @@
 #' 
 list_experiment_types <- function(token)
   request_openbis("listExperimentTypes", token)
+
+#' List experiment ids
+#'
+#' Given a login token, all available experiment ids are listed.
+#' 
+#' @inheritParams logout_openbis
+#' 
+#' @return A a list of ExperimentIdentifier objects.
+#' 
+#' @export
+#' 
+list_experiment_ids <- function(token)
+  request_openbis("listExperiments", token, "IScreeningApiServer")
--- tests/testthat/test-experiment.R
+++ tests/testthat/test-experiment.R
@@ -1,10 +1,19 @@
-context("dataset")
+context("experiment")
 
-test_that("dataset types can be listed", {
+test_that("experiment types can be listed", {
   exp_type <- list_experiment_types(tok)
   expect_is(exp_type, "ExperimentType")
   expect_is(exp_type, "json_vec")
   expect_identical(get_common_subclass(exp_type), "ExperimentType")
   expect_true(all(sapply(exp_type, has_json_subclass, "ExperimentType")))
   expect_gte(length(exp_type), 1L)
+})
+
+test_that("experiment ids can be listed", {
+  exp_ids <- list_experiment_ids(tok)
+  expect_is(exp_ids, "ExperimentIdentifier")
+  expect_is(exp_ids, "json_vec")
+  expect_identical(get_common_subclass(exp_ids), "ExperimentIdentifier")
+  expect_true(all(sapply(exp_ids, has_json_subclass, "ExperimentIdentifier")))
+  expect_gte(length(exp_ids), 1L)
 })
