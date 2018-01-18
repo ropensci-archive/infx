570959720766d52a03a02fc90d182e21a71f69ed
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-18 11:32:45 +0100

    add sample listing

--- R/sample.R
+++ R/sample.R
@@ -1,4 +1,64 @@
 
+#' List samples
+#'
+#' Given a login token, all samples available to the corresponding user on the
+#' queried openBIS instance are listed for an experiment, plate or well. If
+#' multiple objects are used for limiting the search (e.g. a `json_vec` of
+#' wells), an API call for each object has to be made.
+#' 
+#' @section TODO:
+#' Add class specific functions to generic `list_samples()` for
+#' classes `PlateIdentifier` and `WellIdentifier` (as well as `Plate` and
+#' `Well`).
+#' 
+#' @inheritParams logout_openbis
+#' @param x Object to limit the number of returned samples, e.g. a set of
+#' `ExperimentIdentifier` or `Experiment` objects.
+#' @param ... Generic compatibility
+#' 
+#' @export
+#' 
+list_samples <- function(token, x, ...)
+  UseMethod("list_samples", x)
+
+#' @rdname list_samples
+#' @export
+#' 
+list_samples.ExperimentIdentifier <- function(token, x, ...) {
+
+  fields <- c("spaceCode", "projectCode", "experimentCode")
+
+  assert_that(has_fields(x, fields))
+
+  if (!is_json_vec(x))
+    x <- as_json_vec(x)
+
+  exps <- lapply(x, function(y) paste0("/", y[fields], collapse = ""))
+
+  res <- lapply(exps, function(exp)
+    request_openbis("listSamplesForExperiment", list(token, exp)))
+
+  as_json_vec(do.call(c, res))
+}
+
+#' @rdname list_samples
+#' @export
+#' 
+list_samples.Experiment <- function(token, x, ...) {
+
+  assert_that(has_fields(x, "identifier"))
+
+  if (!is_json_vec(x))
+    x <- as_json_vec(x)
+
+  exps <- lapply(x, `[[`, "identifier")
+
+  res <- lapply(exps, function(exp)
+    request_openbis("listSamplesForExperiment", list(token, exp)))
+
+  as_json_vec(do.call(c, res))
+}
+
 #' List sample types
 #'
 #' Given a login token, all available sample types are listed.
--- tests/testthat/test-sample.R
+++ tests/testthat/test-sample.R
@@ -1,5 +1,27 @@
 context("sample")
 
+test_that("samples can be listed", {
+  exp_ids <- list_experiment_ids(tok)
+  experiment <- list_experiments(tok, exp_ids[1:2])
+
+  samp_1 <- list_samples(tok, exp_ids[[1]])
+  expect_is(samp_1, "Sample")
+  expect_is(samp_1, "json_vec")
+  expect_identical(get_common_subclass(samp_1), "Sample")
+  expect_true(all(sapply(samp_1, has_json_subclass, "Sample")))
+  expect_gte(length(samp_1), 1L)
+
+  samp_2 <- list_samples(tok, exp_ids[c(1, 2)])
+  expect_is(samp_2, "Sample")
+  expect_is(samp_2, "json_vec")
+  expect_identical(get_common_subclass(samp_2), "Sample")
+  expect_true(all(sapply(samp_2, has_json_subclass, "Sample")))
+  expect_gte(length(samp_2), 1L)
+
+  expect_identical(samp_1, list_samples(tok, experiment[[1]]))
+  expect_identical(samp_2, list_samples(tok, experiment[1:2]))
+})
+
 test_that("sample types can be listed", {
   samp_types <- list_sample_types(tok)
   expect_is(samp_types, "SampleType")
