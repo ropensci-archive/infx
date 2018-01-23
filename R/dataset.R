3e148549ea91c88a9a44f4cd1b0b32b312298ab9
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-23 11:21:49 +0100

    add ds lister for samples

--- R/dataset.R
+++ R/dataset.R
@@ -1,4 +1,60 @@
 
+#' List datasets
+#'
+#' Given a login token, all available datasets are listed.
+#' 
+#' @inheritParams logout_openbis
+#' 
+#' @section TODO: The API function `listDataSetsForSample()` has a parameter
+#' `areOnlyDirectlyConnectedIncluded`, which is currently fixed to `TRUE`. The
+#' documentation contains the following explanation:
+#' 
+#' > If true, only data sets that are directly connected to the sample are
+#' included, otherwise data sets of child samples are included as well.
+#' 
+#' This does however not seem to correspond to including child datasets in the
+#' API call to `listDataSets()` via its `connectionsToGet` argument. As long
+#' as it is not entirely clear how the inclusion of child/parent datasets
+#' differs from setting `areOnlyDirectlyConnectedIncluded` to `FALSE`, this
+#' option is not exposed to the user.
+#' 
+#' @export
+#' 
+list_datasets <- function(token, x, ...)
+  UseMethod("list_datasets", x)
+
+#' @rdname list_datasets
+#' @export
+#' 
+list_datasets.Sample <- function(token,
+                                 x,
+                                 include = c(NA, "children", "parents", "all"),
+                                 ...) {
+
+  include <- match.arg(include)
+  x <- remove_null(x)
+
+  if (is.na(include)) {
+
+    if (!is_json_vec(x))
+      request_openbis("listDataSetsForSample", list(token, x, TRUE))
+    else
+      request_openbis("listDataSets", list(token, x))
+
+  } else {
+
+    if (include == "all")
+      include <- list("CHILDREN", "PARENTS")
+    else
+      include <- list(toupper(include))
+
+    if (!is_json_vec(x))
+      x <- as_json_vec(x)
+
+    request_openbis("listDataSets", list(token, x, include))
+  }
+}
+
 #' List dataset types
 #'
 #' Given a login token, all available dataset types are listed.
--- tests/testthat/test-dataset.R
+++ tests/testthat/test-dataset.R
@@ -1,5 +1,32 @@
 context("dataset")
 
+test_that("datasets can be listed", {
+  exp_ids <- list_experiment_ids(tok)
+  samples <- list_samples(tok, plate_1[c(1, 2)])
+
+  ds_1 <- list_datasets(tok, samples[[1]])
+  expect_is(ds_1, "DataSet")
+  expect_is(ds_1, "json_vec")
+  expect_identical(get_common_subclass(ds_1), "DataSet")
+  expect_true(all(sapply(ds_1, has_json_subclass, "DataSet")))
+  expect_gte(length(ds_1), 1L)
+
+  ds_2 <- list_datasets(tok, samples[c(1, 2)])
+  expect_is(ds_2, "DataSet")
+  expect_is(ds_2, "json_vec")
+  expect_identical(get_common_subclass(ds_2), "DataSet")
+  expect_true(all(sapply(ds_2, has_json_subclass, "DataSet")))
+  expect_gte(length(ds_2), length(ds_1))
+
+  ds_3 <- list_datasets(tok, samples[[1]], "all")
+  expect_is(ds_3, "DataSet")
+  expect_is(ds_3, "json_vec")
+  expect_identical(get_common_subclass(ds_3), "DataSet")
+  expect_true(all(sapply(ds_3, has_json_subclass, "DataSet")))
+  expect_gte(length(ds_3), length(ds_2))
+})
+
+
 test_that("dataset types can be listed", {
   ds_types <- list_dataset_types(tok)
   expect_is(ds_types, "DataSetType")
