2339b5f2ed8bbce64f31a50d7b2d627889dede21
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-01-31 18:25:46 +0100

    enable material listing for plates

--- R/material.R
+++ R/material.R
@@ -30,22 +30,86 @@
 #' example are identified with Entrez gene ids (e.g. 2475 for MTOR), while for
 #' compounds, a manufacturer name is used.
 #' 
+#' Whenever `list_material()` is dispatched on a (set of) material id
+#' object(s), a (set of) `MaterialGeneric` object(s) is returned. However if
+#' the dispatch occurs on plate objects (`Plate`, `PlateIdentifier` or
+#' `PlateMetadata`), a (set of) `PlateWellMaterialMapping` objects is returned.
+#' If `material_type` is not specified (i.e. `NULL`), the `mapping` field in
+#' the returned object might contain `NULL` for each well. While not mentioned
+#' in the openBis documentation, it is assumed that this occurs in a one to
+#' many mapping scenario (e.g. a well being linked to both an oligo and a
+#' gene). This can be overcome by specifying the type of the material of
+#' interest (see examples).
+#' 
 #' @inheritParams logout_openbis
 #' @param x A (vector of) MaterialIdentifier object(s).
 #' @param code The material code for which an id object is created.
 #' @param type The material type (possible values depend on mode).
 #' @param mode Switch between generic and screening material id objects.
+#' @param material_type A `MaterialTypeIdentifierScreening` object to restrict
+#' the material listing to a certain type of materials.
+#' 
+#' @examples
+#' \dontrun{
+#'   tok <- login_openbis("username", "password")
+#'   exp_ids <- list_experiment_ids(tok)
+#'   plates <- list_plates(tok, exp_ids[[1]])
+#'
+#'   # contains (width x height) times NULL in mapping field
+#'   mat <- list_material(tok, plates[[2]])
+#'   print(mat[[1]], 2L)
+#'   
+#'   # we now restrict the listing to genes
+#'   genes <- json_class(materialTypeCode = "GENE",
+#'                       class = "MaterialTypeIdentifierScreening")
+#'   
+#'   mat <- list_material(tok, plates[[2]], genes)
+#'   print(mat[[1]]$mapping[[1]][[1]], 2L)
+#' }
 #' 
 #' @export
 #' 
-list_material <- function(token, x) {
+list_material <- function(token, x, ...)
+  UseMethod("list_material", x)
 
-  if (!is_json_vec(x))
-    x <- as_json_vec(x)
+#' @rdname list_material
+#' @export
+#' 
+list_material.MaterialIdentifierGeneric <- function(token, x, ...)
+  request_openbis("getMaterialByCodes", list(token, as_json_vec(x)))
 
-  assert_that(all(sapply(x, has_subclass, "MaterialIdentifierGeneric")))
+#' @rdname list_material
+#' @export
+#' 
+list_material.PlateIdentifier <- function(token,
+                                          x,
+                                          material_type = NULL,
+                                          ...) {
 
-  request_openbis("getMaterialByCodes", list(token, x))
+  list_plate_mat_map(token, x, material_type)
+}
+
+#' @rdname list_material
+#' @export
+#' 
+list_material.PlateMetadata <- function(token, x, material_type = NULL, ...)
+  list_plate_mat_map(token, x, material_type)
+
+#' @rdname list_material
+#' @export
+#' 
+list_material.Plate <- function(token, x, material_type = NULL, ...)
+  list_plate_mat_map(token, x, material_type)
+
+list_plate_mat_map <- function(token, x, material_type = NULL) {
+
+  if (!is.null(material_type))
+    assert_that(is_json_class(material_type),
+                has_subclass(material_type, "MaterialTypeIdentifierScreening"))
+
+  request_openbis("listPlateMaterialMapping",
+                  list(token, as_json_vec(x), material_type),
+                  "IScreeningApiServer")
 }
 
 #' @rdname list_material
--- tests/testthat/test-material.R
+++ tests/testthat/test-material.R
@@ -4,7 +4,7 @@ test_that("material ids can be created", {
   material <- material_id(2475L, type = "gene")
   expect_is(material, "MaterialIdentifierGeneric")
   expect_is(material, "json_vec")
-  expect_identical(get_subclass(material), "MaterialIdentifierGeneric")
+  expect_true(has_subclass(material, "MaterialIdentifierGeneric"))
   expect_true(all(sapply(material, has_subclass,
               "MaterialIdentifierGeneric")))
   expect_equal(length(material), 1L)
@@ -13,7 +13,7 @@ test_that("material ids can be created", {
   materials <- material_id(1:3, type = c("gene", "gene", "gene"))
   expect_is(materials, "MaterialIdentifierGeneric")
   expect_is(materials, "json_vec")
-  expect_identical(get_subclass(materials), "MaterialIdentifierGeneric")
+  expect_true(has_subclass(materials, "MaterialIdentifierGeneric"))
   expect_true(all(sapply(materials, has_subclass,
               "MaterialIdentifierGeneric")))
   expect_equal(length(materials), 3L)
@@ -25,8 +25,7 @@ test_that("material ids can be created", {
   material <- material_id(2475L, mode = "screening")
   expect_is(material, "MaterialIdentifierScreening")
   expect_is(material, "json_vec")
-  expect_identical(get_subclass(material),
-                   "MaterialIdentifierScreening")
+  expect_true(has_subclass(material, "MaterialIdentifierScreening"))
   expect_true(all(sapply(material, has_subclass,
               "MaterialIdentifierScreening")))
   expect_equal(length(material), 1L)
@@ -38,16 +37,47 @@ test_that("materials can be listed", {
   mat_1 <- list_material(tok, materials[[1]])
   expect_is(mat_1, "MaterialGeneric")
   expect_is(mat_1, "json_vec")
-  expect_identical(get_subclass(mat_1), "MaterialGeneric")
-  expect_true(all(sapply(mat_1, has_subclass,
-              "MaterialGeneric")))
+  expect_true(has_subclass(mat_1, "MaterialGeneric"))
+  expect_true(all(sapply(mat_1, has_subclass, "MaterialGeneric")))
   expect_equal(length(mat_1), 1L)
 
   mat_2 <- list_material(tok, materials)
   expect_is(mat_2, "MaterialGeneric")
   expect_is(mat_2, "json_vec")
-  expect_identical(get_subclass(mat_2), "MaterialGeneric")
-  expect_true(all(sapply(mat_2, has_subclass,
-              "MaterialGeneric")))
+  expect_true(has_subclass(mat_2, "MaterialGeneric"))
+  expect_true(all(sapply(mat_2, has_subclass, "MaterialGeneric")))
   expect_equal(length(mat_2), 2L)
+
+
+  exp_ids <- list_experiment_ids(tok)
+  plates <- list_plates(tok, exp_ids[[1]])
+  plate_ids <- plate_to_plateid(plates[1:2])
+  plate_meta <- list_plate_metadata(tok, plates[1:2])
+
+  mat_1 <- list_material(tok, plates[[2]])
+  expect_is(mat_1, "PlateWellMaterialMapping")
+  expect_is(mat_1, "json_vec")
+  expect_true(has_subclass(mat_1, "PlateWellMaterialMapping"))
+  expect_true(all(sapply(mat_1, has_subclass, "PlateWellMaterialMapping")))
+  expect_equal(length(mat_1), 1L)
+
+  mat_2 <- list_material(tok, plates[1:2])
+  expect_is(mat_2, "PlateWellMaterialMapping")
+  expect_is(mat_2, "json_vec")
+  expect_true(has_subclass(mat_2, "PlateWellMaterialMapping"))
+  expect_true(all(sapply(mat_2, has_subclass, "PlateWellMaterialMapping")))
+  expect_equal(length(mat_2), 2L)
+
+  expect_identical(list_material(tok, plate_ids[[1]]), mat_1)
+  expect_identical(list_material(tok, plate_ids[1:2]), mat_2)
+
+  expect_identical(list_material(tok, plate_meta[[1]]), mat_1)
+  expect_identical(list_material(tok, plate_meta[1:2]), mat_2)
+
+  genes <- json_class(materialTypeCode = "GENE",
+                      class = "MaterialTypeIdentifierScreening")
+  map_gen <- list_material(tok, plates[[2]], genes)
+
+  expect_true(all(sapply(mat_1[[1]][["mapping"]], is.null)))
+  expect_false(all(sapply(map_gen[[1]][["mapping"]], is.null)))
 })
