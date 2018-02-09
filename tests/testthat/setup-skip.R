5285ddafed7dfce82fbf9ab555b9e1a905dd7753
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-02-09 15:22:07 +0100

    add windows ci

--- tests/testthat/setup-skip.R
+++ tests/testthat/setup-skip.R
@@ -1,15 +1,15 @@
 
 check_skip <- function() {
 
-  if (identical(Sys.getenv("TRAVIS"), "true")) {
-    if (identical(Sys.getenv("TRAVIS_R_VERSION_STRING"), "release"))
-      return(invisible(TRUE))
-    else
-      skip("On non-release Travis")
-  }
+  if (identical(Sys.getenv("TRAVIS"), "true") &&
+      !identical(Sys.getenv("TRAVIS_R_VERSION_STRING"), "release"))
+    skip("On non-release Travis")
 
-  if (identical(Sys.getenv("NOT_CRAN"), "true"))
-    return(invisible(TRUE))
-  else
+  if (identical(Sys.getenv("APPVEYOR"), "True"))
+    skip("On Appveyor")
+
+  if (!identical(Sys.getenv("NOT_CRAN"), "true"))
     skip("On CRAN")
+
+  return(invisible(TRUE))
 }
