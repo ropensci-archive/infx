a32c7a51f2f98b037d885910035de8763b4f4bcf
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-02-14 18:21:55 +0100

    add any property search

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -38,4 +38,14 @@ test_that("search for datasets", {
   expect_identical(get_subclass(ds_1), "DataSet")
   expect_equal(length(ds_1), 1L)
   expect_true(has_subclass(ds_1[[1]], "DataSet"))
+
+  sc <- search_criteria(any_property_match_clause("20715"))
+
+  ds_n <- search_openbis(tok, sc, "data_set")
+
+  expect_is(ds_n, "DataSet")
+  expect_is(ds_n, "json_vec")
+  expect_identical(get_subclass(ds_n), "DataSet")
+  expect_true(all(sapply(ds_n, has_subclass, "DataSet")))
+  expect_gte(length(ds_n), 1L)
 })
