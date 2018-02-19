f5c9ca37b6bc1c84a16efcd1929c06f705a9a8b0
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-02-19 22:15:54 +0100

    remove old file fetcher

--- R/fetch.R
+++ R/fetch.R
@@ -100,58 +100,6 @@ do_download <- function(token,
                   basename(sapply(res, `[[`, "pathInDataSet")))
 }
 
-#' @title Split downloads
-#'
-#' @description A set of files is split into buckets and downloaded
-#' asynchronously and the progress is reported using a progress bar. The
-#' splitting is necessary as [do_download] downloads all files simultaneously.
-#' 
-#' @inheritParams do_download
-#' @param async The bucket size for simultaneous downloads.
-#' 
-#' @return A list of raw vectors holding the downloaded data.
-#' 
-#' @section TODO: entry point to all downloads, choose single or async (both
-#' not exported) and use this in plate/meta fetching.
-#' 
-#' @export
-#' 
-fetch_files <- function(token,
-                        files,
-                        data_id,
-                        async = 5L) {
-
-  assert_that(length(files) >= 1L,
-              all(sapply(files, has_subclass, "FileInfoDssDTO")),
-              length(async) == 1L, is.numeric(async))
-
-  n_bins <- ceiling(length(files) / async)
-  bin_size <- ceiling(length(files) / n_bins)
-
-  cut <- rep(1:n_bins, each = bin_size)[seq_len(length(files))]
-
-  if (n_bins > 1) {
-    tot <- sum(as.integer(sapply(files, `[[`, "fileSize")))
-    pb <- progress::progress_bar$new(
-      format = paste0("downloading [:bar] :percent in :elapsed (:bytes of ",
-                      format(structure(tot, class = "object_size"),
-                             units = "auto"), ")"),
-      total = tot)
-    pb$tick(0)
-  } else pb <- NULL
-
-  res <- lapply(split(files, cut), function(x) {
-    dat <- lapply(do_download(token, data_id, x), function(y) {
-      tryCatch(read_data(y), error = function(e) NULL)
-    })
-    if (!is.null(pb)) pb$tick(sum(as.integer(sapply(x, `[[`, "fileSize"))))
-    dat[!sapply(dat, is.null)]
-  })
-
-  stats::setNames(unlist(res, recursive = FALSE),
-                  unlist(lapply(res, names), recursive = FALSE))
-}
-
 #' @title Fetch single cell data
 #'
 #' @description Download single cell datasets corresponding to a plate barcode
