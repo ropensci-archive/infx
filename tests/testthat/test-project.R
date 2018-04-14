e907cdaa75a0eaa911452921ee8fa4679661e7b0
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-04-14 18:27:38 +0200

    update project tests

--- tests/testthat/test-project.R
+++ tests/testthat/test-project.R
@@ -3,7 +3,9 @@ context("project")
 test_that("projects can be listed", {
   expect_is(projects, "Project")
   expect_is(projects, "json_vec")
-  expect_identical(get_subclass(projects), "Project")
-  expect_true(all(sapply(projects, has_subclass, "Project")))
   expect_gte(length(projects), 1L)
+  for (i in seq_along(projects)) {
+    expect_is(projects[[i]], "Project")
+    expect_is(projects[[i]], "json_class")
+  }
 })
