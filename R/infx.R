39e83c7d345c4f7d2d69884c01018e0da9b29245
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-04-15 22:29:16 +0200

    update pkg docs

--- R/infx.R
+++ R/infx.R
@@ -45,6 +45,89 @@
 #' corresponding to an imaging site. This is typically used for image
 #' acquisition meta data, summarized image analysis or quality control results.
 #' 
+#' @section JSON object handling:
+#' Object structures as returned by openBIS can be instantiated using the
+#' creator [json_class()]. This function takes an arbitrary set of key-value
+#' pairs, followed by a class name and returns a list-based `json_class`
+#' object. Existing list-based objects may be coerced to `json_class` using
+#' [as_json_class()] where `@type` list entries are taken to be class types.
+#' The inverse is achieved by calling [rm_json_class()] on a `json_class`
+#' object or by calling [as_list()] and passing the `keep_asis` argument as
+#' `FALSE`. `json_class` objects can be validated with [is_json_class()] which
+#' is recursively called on any object inheriting from `json_class` in
+#' [check_json_class()].
+#' 
+#' Similarly to `json_class` objects, a constructor for `json_vec` objects is
+#' provided in the form if [json_vec()] and existing structures can be coerced
+#' to `json_vec` by [as_json_vec()]. The validator function [is_json_vec()]
+#' tests whether an object is a properly formed `json_vec` object and the
+#' utility function [has_common_subclass()] tests whether the passed list
+#' structure consists of `json_class` objects of the same sub-type. The inverse
+#' of applying [as_json_vec()] to a list structure is achieved by passing a
+#' `json_vec` object to [as_list()].
+#' 
+#' Several utility functions are provided that facilitate handling of
+#' `json_class` and `json_vec` objects. [has_fields()] tests whether certain
+#' named entries are present in a `json_class` object or in each member of a
+#' `json_vec`. In order to extract the content of a field, [get_field()] can be
+#' applied to `json_class` and `json_vec` objects. Analogously,
+#' [has_subclass()] and [get_subclass()] test for and extract the original JSON
+#' object type from `json_class` and `json_vec` objects. Finally,
+#' [remove_null()] recursively removes empty fields (fields containing `NULL`)
+#' from `json_class` and `json_vec` objects.
+#' 
+#' In addition to the mentioned utility functions, several base R generic
+#' functions have `json_class` and `json_vec` specific methods implemented.
+#' Combining several `json_class` objects using [base::c()] yields a `json_vec`
+#' object, as does repeating objects using [base::rep()]. The same functions
+#' can be applied to `json_vec` objects but this only checks for agreement in
+#' sub-type. Custom sum-setting is provided as well, in order to retain class
+#' attributes and replacement functions acting on `json_vec` objects make sure
+#' that sub-types remain compatible. Recursive printing of both `json_class`
+#' and `json_vec` objects is possible by calling [base::print()]. Recursion
+#' depth, as well as printing length and width can be controlled via arguments,
+#' as can fancy printing (colors and UTF box characters for visualizing tree
+#' structures) be disabled.
+#' 
+#' @section Listing and searching for objects:
+#' OpenBIS projects can be listed by calling [list_projects()] and experiments
+#' are enumerated with [list_experiments()]. Two objects types are used to
+#' represent experiments: `Experiment` and `ExperimentIdentifier`.
+#' [exp_to_expid()] converts a set of `Experiment` objects to
+#' `ExperimentIdentifier` and the inverse is possible by passing a set of
+#' `ExperimentIdentifier` objects to [list_experiments()]. All available
+#' experiments can be listed as `ExperimentIdentifier` objects using
+#' [list_experiment_ids()] and all experiments for a set of projects are
+#' enumerated by passing `Project` objects to [list_experiments()]. Experiments
+#' have a type and all realized types can be listed with
+#' [list_experiment_types()].
+#' 
+#' Experiments consist of samples which are listed by passing a set of
+#' `Experiment` or `ExperimentIdentifier` objects to [list_samples()]. Samples
+#' too have a type and all types are retrieved with [list_sample_types()].
+#' Additional object types that are used to represent samples are plate and
+#' well objects, including `Plate`, `PlateIdentifier`, `PlateMetadata`,
+#' `WellIdentifier` and `WellMetadata`, all of which can be converted to
+#' `Sample` objects by calling [list_samples()]. Plate objects can be listed
+#' using [list_plates()], which can either return all available plate objects
+#' or plates for a given set of experiments (passed as `Experiment` or
+#' `ExperimentIdentifier` objects). Plate meta data, which also contains
+#' associated well meta data is retrieved by [list_plate_metadata()] which can
+#' act on plate objects (`Plate`, `PlateIdentifier` or `Sample`). Wells of a
+#' plate are listed with [list_wells()] which too may be dispatched on plate
+#' objects. Wells associated with a material object can be enumerated by
+#' passing a set of `MaterialScreening`, `MaterialIdentifierScreening`,
+#' `MaterialGeneric` or `MaterialIdentifierGeneric` to [list_wells()].
+#' 
+#' Data set objects represent the most diverse group of data-organizational
+#' structures. Possibly types include `DataSet`, `DatasetIdentifier`,
+#' `DatasetReference`, `ImageDatasetReference`, `MicroscopyImageReference`,
+#' `PlateImageReference`, `FeatureVectorDatasetReference` and
+#' `FeatureVectorDatasetWellReference`.
+#' 
+#' @section Downloading data:
+#' The foo functions ...
+#'
 #' @docType package
 #' @name infx
 #' 
