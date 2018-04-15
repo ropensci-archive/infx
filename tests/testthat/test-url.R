0e3a6aa3be9e01707d4a3932bd2503725e2357fe
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-04-15 12:47:58 +0200

    move docs url tests

--- tests/testthat/test-url.R
+++ tests/testthat/test-url.R
@@ -135,3 +135,24 @@ test_that("dataset download urls can be generated", {
   expect_length(url_to, 1L)
   expect_true(grepl("^https://", url_to))
 })
+
+test_that("openbis api urls and docs links can be generated", {
+  url <- api_url()
+  expect_is(url, "character")
+  expect_length(url, 1L)
+  expect_match(url, "^https://")
+  expect_identical(url, api_url("gis"))
+
+  link <- docs_link()
+  expect_is(link, "character")
+  expect_length(link, 1L)
+  expect_match(link, "^\\\\href\\{.+\\}\\{.+\\}$")
+  expect_identical(link, docs_link("gis"))
+
+  link <- docs_link(method_name = "foo")
+  expect_is(link, "character")
+  expect_length(link, 1L)
+  expect_match(link, "^\\\\href\\{.+\\}\\{.+\\:foo}$")
+  expect_identical(link, docs_link("gis", method_name = "foo"))
+})
+
